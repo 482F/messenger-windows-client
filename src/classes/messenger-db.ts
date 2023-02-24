bf05c473c90d6e622ebafb9b2b9ffa7a6c51aec6
482F <482F.git.mail@482f.net>
2023-02-25 00:00:00 +0900

    メッセージの作成などを MessengerDb 内で行うように

--- src/classes/messenger-db.ts
+++ src/classes/messenger-db.ts
@@ -1,9 +1,10 @@
-import { getDb } from '../utils/sql'
+import { getDb, EDatabase } from '../utils/sql'
 
 const initializeQueries = [
+  // query は JSON 形式だが、tauri-plugin-sql が JSON をサポートしていないためこちらでパースする
   `CREATE TABLE IF NOT EXISTS queries (
     id          INTEGER  UNIQUE NOT NULL PRIMARY KEY  ,
-    query       JSON            NOT NULL              ,
+    query       TEXT            NOT NULL              ,
     time        DATETIME        NOT NULL
   );`,
   `CREATE TABLE IF NOT EXISTS users (
@@ -19,8 +20,49 @@ const initializeQueries = [
   );`,
 ]
 
-export async function getMessengerDb() {
-  const db = await getDb()
-  await Promise.all(initializeQueries.map((query) => db.execute(query)))
-  return db
+export class MessengerDb {
+  db: EDatabase
+  constructor(db: EDatabase) {
+    this.db = db
+  }
+  static async load() {
+    const db = await getDb()
+    await Promise.all(initializeQueries.map((query) => db.execute(query)))
+    return new MessengerDb(db)
+  }
+  async getAllMessages() {
+    return this.db.select<{
+      id: number
+      body: string
+      userId: number
+      time: string
+      name: string
+    }>(`
+      SELECT
+        *
+      FROM messages
+        INNER JOIN users
+        ON messages.user_id = users.id;`)
+  }
+  async createMessage(userId: number, body: string) {
+    const now = new Date()
+    const { lastInsertId: postId } = await this.db.execute(
+      `INSERT INTO messages (body, user_id, time) VALUES (?, ?, ?);`,
+      [body, userId, now]
+    )
+    await this.db.execute(`INSERT INTO queries (query, time) VALUES (?, ?);`, [
+      JSON.stringify({
+        userId,
+        type: 'post.create',
+        payload: {
+          postId,
+          body,
+          // parentPostIds,
+          // childPostIds,
+          // tagIds,
+        },
+      }),
+      now,
+    ])
+  }
 }
--- src/components/main-content.vue
+++ src/components/main-content.vue
@@ -23,29 +23,25 @@ TODO:
 - user 登録の実装
   - 公開鍵、秘密鍵を作成して複合鍵暗号通信の実装
 - INSERT 等はクラスのメソッドに隠す
+- 投稿の id は uuid v4 にする
 - db に同時刻同メッセージの投稿を許容しないようにする
   - 同一 PC で動いている時に同じ内容が書き込まれてしまうため
 - 同期処理の実装
 */
 import ATextField from './atoms/a-text-field.vue'
 import { ref } from 'vue'
-import { getMessengerDb } from '../classes/messenger-db'
+import { MessengerDb } from '../classes/messenger-db'
 
-const db = await getMessengerDb()
+const db = await MessengerDb.load()
 console.log(db)
 
 const input = ref('')
 const text = ref('')
 
-const records = await db.select<{
-  id: number
-  body: string
-  userId: number
-  time: Date
-}>(`SELECT * FROM messages;`)
+const records = await db.getAllMessages()
 
 text.value = records
-  .map((record) => `${record.userId}: ${record.body}`)
+  .map((record) => `${record.name}: ${record.body}`)
   .join('\n')
 
 console.log({ records })
@@ -58,32 +54,15 @@ async function addText(sender: string, body: string) {
   const userName = sender.slice(0, 5)
   text.value += '\n' + userName + ': ' + body
 
-  const now = new Date()
-  await db.execute(`INSERT OR IGNORE INTO users (name) VALUES (?);`, [userName])
-  const userId: string =
-    (await db
-      .select<{ id: string }>(`SELECT id FROM users WHERE name = ?;`, [
-        userName,
-      ])
-      .then((r) => r[0]?.id)) ?? ''
-  const { lastInsertId: postId } = await db.execute(
-    `INSERT INTO messages (body, user_id, time) VALUES (?, ?, ?);`,
-    [body, userId, now]
-  )
-  await db.execute(`INSERT INTO queries (query, time) VALUES (?, ?);`, [
-    JSON.stringify({
-      userId,
-      type: 'post.create',
-      payload: {
-        postId,
-        body,
-        // parentPostIds,
-        // childPostIds,
-        // tagIds,
-      },
-    }),
-    now,
-  ])
+  const userId = 1
+  db.createMessage(userId, body)
+  // await db.execute(`INSERT OR IGNORE INTO users (name) VALUES (?);`, [userName])
+  // const userId: string =
+  //   (await db
+  //     .select<{ id: string }>(`SELECT id FROM users WHERE name = ?;`, [
+  //       userName,
+  //     ])
+  //     .then((r) => r[0]?.id)) ?? ''
 }
 connection.value.onmessage = function (event) {
   console.log({ event })
